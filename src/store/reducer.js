02a1d901fa5c72280c6ef0275187b615a4a1016f
Russkazkin <ruslan@skazkin.su>
2021-02-28 09:35:04 +0300

    Add my solution

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,29 @@
+import * as actionTypes from "./actions";
+
 const initialState = {
     persons: [],
 }
 
 const reducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case actionTypes.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(),
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
+            return {
+                ...state,
+                persons: state.persons.concat(newPerson),
+            }
+        case actionTypes.DELETE_PERSON:
+            return {
+                ...state,
+                persons: state.persons.filter((person) => person.id !== action.id),
+            };
+        default:
+            return state;
+    }
 }
 
 export default reducer;
